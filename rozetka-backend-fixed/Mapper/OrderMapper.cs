5457b5bda9b4f5a28e18ccd789fc7d2f2c2ec223
valent2004 <valentyn2004pro@gmail.com>
2025-09-21 21:51:54 +0300

    Add Order Models

---
+++ rozetka-backend-fixed/Mapper/OrderMapper.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using rozetkabackend.Data.Entities.Catalog;
+using rozetkabackend.Models.Order;
+using System.Linq;
+
+namespace rozetkabackend.Mapper;
+
+public class OrderMapper : Profile
+{
+    public OrderMapper()
+    {
+        CreateMap<OrderItemEntity, OrderItemModel>()
+            .ForMember(x => x.ProductImage, opt => opt
+                .MapFrom(x => x.Product!.ProductImages!.OrderBy(x => x.Priority).First().Name))
+            .ForMember(x => x.ProductName, opt => opt.MapFrom(x => x.Product!.Name))
+            .ForMember(x => x.ProductSlug, opt => opt.MapFrom(x => x.Product!.Slug));
+
+        CreateMap<OrderEntity, OrderModel>()
+            .ForMember(x => x.Status, opt => opt.MapFrom(x => x.OrderStatus!.Name));
+
+        CreateMap<CartEntity, OrderItemEntity>()
+            .ForMember(x => x.PriceBuy, opt => opt
+            .MapFrom(x => x.Product!.Price))
+            .ForMember(x => x.Count, opt => opt
+            .MapFrom(x => x.Quantity));
+
+        //CreateMap<DeliveryInfoCreateModel, DeliveryInfoEntity>();
+    }
+}
---
+++ rozetka-backend-fixed/Models/Order/OrderModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace rozetkabackend.Models.Order;
+
+public class OrderModel
+{
+    public long Id { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public DateTime DateCreated { get; set; }
+    public decimal TotalPrice { get; set; }
+    public List<OrderItemModel> OrderItems { get; set; }
+}
